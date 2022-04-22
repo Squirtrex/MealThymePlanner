6a2c25e9f725d1cb3a1dc51efa90cd505e3e8806
Nick Pepin <31704762+Warpeped@users.noreply.github.com>
2022-04-22 00:07:52 -0400

    Update IngredientTag.java

--- src/com/mycompany/mealthymeplanner/IngredientTag.java
+++ src/com/mycompany/mealthymeplanner/IngredientTag.java
@@ -1,6 +1,6 @@
 package com.mycompany.mealthymeplanner;
 public enum IngredientTag {
     
-    Vegan, Vegetarian, Gluten, Gluten_Free, Chicken, Beef, Pork, Meat, Shellfish, Fish;
+    Vegan, Vegetarian, Gluten, Gluten_Free, Chicken, Beef, Pork, Meat, Shellfish, Fish, Dairy, Eggs, Dairy_Free;
 
 }
